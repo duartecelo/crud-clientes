df6deb16a3ac9f3a2fffdd616b0c6af6f85bf629
Marcelo Duarte <celo.duarte.2005@gmail.com>
2025-03-18 02:56:19 -0300

    Criação e dependências

---
+++ src/test/java/com/duartecelo/crud_clientes/CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duartecelo.crud_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
