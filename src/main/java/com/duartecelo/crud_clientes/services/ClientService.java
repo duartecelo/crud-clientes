54835a7fc302df1924d82d41a56a813a10d3933d
Marcelo Duarte <celo.duarte.2005@gmail.com>
2025-03-19 01:25:27 -0300

    ClientService

---
+++ src/main/java/com/duartecelo/crud_clientes/services/ClientService.java
@@ -0,0 +1,65 @@
+package com.duartecelo.crud_clientes.services;
+
+import com.duartecelo.crud_clientes.entities.Client;
+import com.duartecelo.crud_clientes.repositories.ClientRepository;
+import com.duartecelo.crud_clientes.services.exceptions.DataBaseException;
+import com.duartecelo.crud_clientes.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    @Transactional
+    public Client findById(Long id) {
+        return clientRepository
+                .findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
+    }
+
+    @Transactional
+    public Page<Client> findAll(Pageable pageable) {
+        return clientRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public Client insert(Client client) {
+        return clientRepository.save(client);
+    }
+
+    @Transactional
+    public Client update(Long id, Client updatedClient) {
+        try {
+            Client originClient = clientRepository.getReferenceById(id);
+
+            originClient.setName(updatedClient.getName());
+            originClient.setCpf(updatedClient.getCpf());
+            originClient.setIncome(updatedClient.getIncome());
+            originClient.setBirthDate(updatedClient.getBirthDate());
+            originClient.setChildren(updatedClient.getChildren());
+
+            return originClient;
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id) {
+        if (!clientRepository.existsById((id))) throw new ResourceNotFoundException("Recurso não encontrado");
+        try {
+            clientRepository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DataBaseException("Falha de integridade referencial");
+        }
+    }
+}
