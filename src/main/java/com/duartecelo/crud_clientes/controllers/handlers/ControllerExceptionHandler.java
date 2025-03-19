519fa8801acd66f408f830b8b8485156c119db5b
Marcelo Duarte <celo.duarte.2005@gmail.com>
2025-03-19 01:26:03 -0300

    Errors, Exceptions, Handlers

---
+++ src/main/java/com/duartecelo/crud_clientes/controllers/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.duartecelo.crud_clientes.controllers.handlers;
+
+import com.duartecelo.crud_clientes.dto.CustomError;
+import com.duartecelo.crud_clientes.dto.ValidationError;
+import com.duartecelo.crud_clientes.services.exceptions.DataBaseException;
+import com.duartecelo.crud_clientes.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DataBaseException.class)
+    public ResponseEntity<CustomError> database(DataBaseException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+        for (FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(err);
+    }
+}
---
+++ src/main/java/com/duartecelo/crud_clientes/services/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.duartecelo.crud_clientes.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+    public DataBaseException(String msg) {
+        super(msg);
+    }
+}
